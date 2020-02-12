3cf2ccb558de3211e18f89480a9f6e466f04e090
Lev Riabov <levriabov12345@mail.ru>
2020-02-12 21:35:13 +0300

    шаг 1

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 4200,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+    }),
+  ],
+};
